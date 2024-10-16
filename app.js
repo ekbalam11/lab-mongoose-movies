21b805d63f6022b584b07a391c5435beff7501f5
Balam Castro <64442582+ekbalam11@users.noreply.github.com>
2024-10-16 22:10:38 +0200

    index.ejs and get celebrities config

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config()
 
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -8,21 +9,24 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const router       = require('./routes/celebrities.js')
+console.log("🚀 ~ router :", router )
+
+async function connectDB() {
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log('Connected to the database');
+};
+connectDB().catch(err => console.log(err));
 
 
-mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
+
+
+
 
 // Middleware Setup
 app.use(logger('dev'));
@@ -31,10 +35,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-      
+//EJS as template engine    
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
@@ -47,6 +51,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index');
 app.use('/', index);
+app.use('/celebrities', router.getCelebrities);
 
 
-module.exports = app;
+
+module.exports = app
